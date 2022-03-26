23089230a0ecc5acd8153c5757fe49deda55dd6e
nicholas-ewasiuk <ewasiuk.nick@gmail.com>
2022-03-25 21:59:24 -0700

    antd component layout

--- src/Body.tsx
+++ src/Body.tsx
@@ -1,26 +1,59 @@
 /** @jsxImportSource @emotion/react */
 import React from 'react';
 import { css } from '@emotion/react';
-import { Button, Col, Row } from 'antd';
+import { Button, Col, Collapse, Row } from 'antd';
 import Layout, { Content, Header } from 'antd/lib/layout/layout';
+import Sider from 'antd/lib/layout/Sider';
 
 export const Body: React.FC = () => {
+  const { Panel } = Collapse;
+
   return (
     <Layout>
-      <Header>
-        header
-      </Header>
-      <Content>
-        <Row>
-          <Col>Content</Col>
-        </Row>
-        <Row>
-          content
-        </Row>
-        <Row>
-          content
-        </Row>
-      </Content>
+      <Sider>
+      </Sider>
+      <Layout>
+        <Header>
+          Navbar
+        </Header>
+        <Content>
+          <Row>
+            <Col span={24}>
+              <div id="funds">
+                <Collapse 
+                  bordered={false}
+                  expandIconPosition='right'
+                  ghost={true}
+                >
+                  <Panel
+                    header={
+                      <div
+                        className='icon-header'
+                      >
+                        <img src="" />
+                        <span>Managed Funds</span>
+                        <span>(Investin data here)</span>
+                      </div>
+                    } 
+                    role="button" 
+                    key="1"
+                  >
+                    <p>
+                      placeholder
+                    </p>
+                  </Panel>
+                </Collapse>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            content
+          </Row>
+          <Row>
+            content
+          </Row>
+        </Content>
+      </Layout>
     </Layout>
   );
 };
